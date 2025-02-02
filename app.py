3492bccadf8be110cab1e4ce964b51b4fa494af2
Mevv04 <mevanf.solanga@gmail.com>
2025-02-02 10:53:41 -0500

    heartrate feed fed into flask

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, Response
 import cv2
 from face_monitor.main import process_frame
+import threading
+from app_utils import setup_serial
 
 app = Flask(__name__)
+
+
 
 # Initialize webcam
 camera = cv2.VideoCapture(0)
@@ -15,7 +19,6 @@ def index():
 def generate_frames():
     while True:
         success, frame = camera.read()
-        # print(camera.read())
         if not success:
             print('test')
             break
@@ -34,6 +37,15 @@ def generate_frames():
 def video_feed():
     # Video streaming route
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Start the serial communication in a separate thread
+def start_serial_communication():
+    setup_serial()
+    # print(bpminfo)
+
+serial_thread = threading.Thread(target=start_serial_communication)
+serial_thread.daemon = True
+serial_thread.start()
 
 if __name__ == '__main__':
     app.run(debug=True)
